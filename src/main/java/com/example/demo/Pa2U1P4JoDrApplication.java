486de12c3dcbe98b97fd7b7c8d4971d15878c09f
Josue Ocapana <josuensd@gmail.com>
2023-05-15 19:36:11 -0500

    Taler 4

--- src/main/java/com/example/demo/Pa2U1P4JoDrApplication.java
+++ src/main/java/com/example/demo/Pa2U1P4JoDrApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +20,15 @@ public class Pa2U1P4JoDrApplication implements CommandLineRunner{
 		/*Spring Framework: esto es un framework que permite la 
 		construccion/desarrollo de aplicaciones empresariales basadas en spring
 		*/
+		Profesor p = new Profesor();
+		
+		p.setNombre("Diego");
+		p.setApellido("Rivas");
+		p.setCedula("1725051146");
+		p.setFech_nacimineto(LocalDateTime.now());
+		
+		System.out.print(p);
+		
 	}
 
 }
